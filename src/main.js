c23fc7a070914d535540fb7af5cb95a0072b949a
blytheSchen <blschen@ucsc.edu>
2025-01-14 12:57:05 -0800

    copied over wfdm from initial demo

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let config = {
 	render: {
 		pixelArt: true	// prevent pixel art from getting blurred when scaled
 	},
-	scene: [Generator]
+	scene: [Wfdm]
 }
 
 const game = new Phaser.Game(config);
