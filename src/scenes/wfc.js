8348839a7f9e46713edcf62ca23b9f762ac96bd9
corvuscorae <corvuscorae@gmail.com>
2025-01-23 00:17:25 -0800

    wfc debugged + some cleanup

--- src/scenes/wfc.js
+++ src/scenes/wfc.js
@@ -1,44 +1,156 @@
-class WFC // extends Phaser.Scene
-{
-	constructor(ip, size) {
-		//super("wfcScene");
-		this.ip = ip;
-		this.size = size;
+// this class handles all of the heavy lifting for generating a WFC grid
+// the only parameter available outside of the class is the resulting grid
+class WFC {
+	// private properties
+	#ip;
+	#size;
+	#patternAdjs;
+	#grid;
+	#patternOptions;
+	#uncollapsed;
 
-		if(this.init()){
+	constructor(ip, size) {
+		this.#ip = ip;
+		this.#size = size;
+
+		if(this.#run()){
 			console.log("WFC finished!");
-			console.log(this.grid);
-			console.log(this.patternAdjs);
-			this.printGrid();	// print pattern choices in console
+			////console.log(this.#patternAdjs)
+			this.#printGrid();
+
+			// output accesible outside of class
+			this.generated = this.#patternsToTiles();	// converts grid from pattern to tiles
 		}
 	}
 
-	init(){
+	/*** MAIN WFC FUNCTIONS ***/
+
+	// run() is the command center of the WFC algorithm
+	#run(){
 		console.log("initializing WFC...");
 
 		// initialize cell grid 
-		// each cell of the grid represents an N x N subset
-		this.patternOptions = Array.from({ length: this.ip.patterns.length }, (_, i) => i);
-		this.grid = this.initGrid(this.size, this.patternOptions);
+		// 		each cell starts with all patterns as its options (uncollapsed)
+		this.#patternOptions = Array.from({ length: this.#ip.patterns.length }, (_, i) => i);
+		this.#grid = this.#initGrid(this.#size, this.#patternOptions);
 
-		// reorganize this.ip.adjacencies by pattern and direction
-		//		makes something like:
-		//		[ 0: {up: [...], down: [...] }, 1: { down: [...], right: [...] } ]
-		this.patternAdjs = this.adbjByPattern(this.patternOptions);
+		// reorganize this.#ip.adjacencies by pattern and direction
+		//		this will help in the propagation phase
+		this.#patternAdjs = this.#adjByPattern(this.#patternOptions);
 
-		this.collapsed = this.initGrid(this.size);	// empty grid to track collapsed cells
-		this.uncollapsed = [...this.grid];			// copy of this.grid to help with observe phase
+		this.#uncollapsed = [...this.#grid];				// copy of this.#grid to help with observe phase
 
-		let cont = true;
-		while(this.uncollapsed.length > 0){
-			cont = this.run();
-			if(!cont) this.init();
+		// loops until WFC is completed
+		let cont = true; 								// bool to track whether to continue WFC	
+		while(this.#uncollapsed.length > 0){
+			let cell = this.#observe();					// picks a random uncollapsed cell
+			let pick = this.#randomIndex(cell.options);	// pick one option of observed's options
+
+			cell.options = [cell.options[pick]];
+
+			this.#resetVisitFlag();						// sets all visit flags in grid to false
+			cont = this.#propagate(cell);
+
+			if(!cont) this.#run();						// false indicates WFC gridlock; restart
 		}
 
-		return true;
+		return true; // WFC completed! wahoo!!
 	}
 
-	initGrid(size, options) {
+	// observe phase -- picks a random uncollapsed cell
+	//		TODO: add functionality to get minimun entropy
+	#observe(){
+		let pickIndex = this.#randomIndex(this.#uncollapsed);
+		let cell = this.#uncollapsed[pickIndex];
+		cell.collapsed = true;								// collapse cell
+
+		let originalIndex = (cell.y * this.#size) + cell.x;	// get cell's original index
+		this.#grid[originalIndex].collapsed = true;			// update grid at original index
+		this.#uncollapsed.splice(pickIndex, 1);				// remove cell from uncollapsed
+
+		return cell;
+	}
+
+	// propagate startCell's option choice to all other cells
+	#propagate(startCell){
+		const stack = [startCell]; 		// use a stack to track which cells' options need to be propagated
+		while (stack.length > 0) {
+			const cell = stack.pop();
+
+			// update all of cell's neighbors
+			for(let dir in cell.neighbors){
+				let neighborAddress = cell.neighbors[dir];
+				if(neighborAddress === null){ continue; }			// cell doesnt have a neighbor in this direction
+				
+				let neighbor = this.#grid[neighborAddress]; 
+				if(neighbor.visited === true){ continue; }			// cell has already been visited in this propagation
+				
+				neighbor.visited = true;
+
+				// find valid options for this neighbor by looking at each pattern in cell's options
+				//		and finding which adjacencies are valid for that neighbor
+				let valid = new Set();								// set to prevent duplication
+				for(let cellOption of cell.options){
+					// left neighbor's options == cell tile's left adjacencies, etc
+					let pAdj = this.#patternAdjs[cellOption][dir];	// the adjacencies for this pattern (cellOption)
+					if(!pAdj) continue;								// pattern has no adjacencies in this direction
+					for(let adj of pAdj){	
+						valid.add(adj);	
+					}
+				}
+				// update neighbor's options to be whichever are options are in valid[]
+				let newOptions = valid.intersection(new Set(neighbor.options));
+				neighbor.options = [...newOptions];
+
+				if(neighbor.options.length === 0){	// gridlock :(
+					return false;					// will trigger a restart
+				}
+
+				if(neighbor.options.length === 1){	// this neighbor is collapsed!
+					neighbor.collapsed = true;
+
+					// remove neighbor from this.#uncollapsed
+					for(let i = 0; i < this.#uncollapsed.length; i++){
+						if(this.#uncollapsed[i].x === neighbor.x && this.#uncollapsed[i].y === neighbor.y){
+							this.#uncollapsed.splice(i, 1);
+							break;
+						}
+					}
+				}
+
+				// add neighbor to propagation stack
+				if(neighbor.options.length < this.#patternOptions.length){
+					stack.push(neighbor);
+				} 
+			}
+		}
+		
+		return true; // no deadlocks detected, done with this propagation
+	}
+
+	// conversion function to get the top left tile of every pattern
+	#patternsToTiles(){
+		let result = []
+		for(let cell of this.#grid){
+			let patternIndex = cell.options[0];
+			let pattern = this.#ip.patterns[patternIndex];
+			let tile = pattern[0][0];		// top left tile of pattern
+			
+			result.push(tile);		
+		}
+
+		return result;
+	}
+
+	/*** HELPER FUNCTIONS ***/
+
+	// makes a ( size x size ) grid, initializing each cell with:
+	// 		- (x, y) address
+	//		- collapsed flag set to false
+	//		- visit flag set to false
+	//		- options populated with all patterns
+	//		- neighbors' addresses
+	#initGrid(size, options) {
 		let result = [];
 
 		for(let y = 0; y < size; y++){
@@ -65,138 +177,9 @@ class WFC // extends Phaser.Scene
 
 		return result;
 	}
-
-	run(){
-		let observed = this.observe();
-
-		// pick one option of cell's options
-		let pick = this.randomIndex(observed.cell.options);
-		observed.cell.options = [observed.cell.options[pick]];
-
-		this.resetVisitFlag();
-		return this.propagate(observed);
-	}
-
-	// TODO: get minimun entropy
-	observe(){
-		let pickIndex = this.randomIndex(this.uncollapsed);
-		let cell = this.uncollapsed[pickIndex];
-		cell.collapsed = true;								// collapse cell
-
-		let originalIndex = (cell.y * this.size) + cell.x;	// get cell's original index
-		this.grid[originalIndex].collapsed = true;			// update grid
-		this.collapsed[originalIndex] = cell;				// move cell to collapses
-		this.uncollapsed.splice(pickIndex, 1);				// remove cell from uncollapsed
-		return {cell: cell, index: originalIndex };
-	}
-
-	// TODO: debug; propagate fails every time :(
-	//		could try backtrack instead of restart
-	//		or make sure that whatever choice the startCell had cannot be chosen again for that cell
-	propagate(observed){
-		let startCell = observed.cell;
-
-		const stack = [startCell]; 
-		while (stack.length > 0) {
-			const cell = stack.pop();
-
-			// update all of cell's neighbors
-			for(let dir in cell.neighbors){
-				let neighborAddress = cell.neighbors[dir];
-				if(neighborAddress === null){ continue; }	// cell doesnt have a neighbor in this direction
-				
-				let neighbor = this.grid[neighborAddress]; 
-				//if(neighbor.collapsed === true){ continue; }
-				if(neighbor.visited === true){ continue; }
-				neighbor.visited = true;
-
-				// NEW VERSION: writes valid options to neighbor.options
-				// issue -- outputting a grid with invalid adjacencies
-				//		 - technically though the adjacencies would be valid with rotation
-				// left neighbor's options == cell tile's left adjacencies, etc
-				let valid = new Set();	// set to prevent duplication
-				for(let cellOption of cell.options){
-					let pAdj = this.patternAdjs[cellOption][dir];	// the adjacencies for this pattern (cellOption)
-					if(!pAdj) continue;								// pattern has no adjacencies in this direction
-					for(let adj of pAdj){	
-						valid.add(adj);	
-					}
-				}
-				// find which of neighbor's options are still valid
-				// issue -- if i comment the following two lines out, it works but WFC outputs
-				//				a grid with invalid adjacencies present
-				//		 -- if i leave it, sometimes gets the error that there are too many recursions
-				//		  - also still seems to output invalid adjs :(
-				let newOptions = valid.intersection(new Set(neighbor.options));
-				neighbor.options = [...newOptions];
-
-				/*
-				// OLD VERSION: deletes invalid options
-				// cull invalid options from neighbor.options
-				let cull = [];
-				for(let cellOption of cell.options){
-					let filterDir;
-					switch(dir){
-						// if neighbor is cell's up, then neighbor's options must all have
-						//		cell as a valid down, etc...
-						case "up": filterDir = "down"; break;
-						case "down": filterDir = "up"; break;
-						case "left": filterDir = "right"; break;
-						case "right": filterDir = "left"; break;
-					}
-					//cull = cull.concat(this.cullOptions(cellOption, filterDir, neighbor));
-
-				}
-				*/
-
-				/*
-				// remove cull elements from neighbor.options
-				let len = neighbor.options.length;
-				for(let i = len - 1; i >= 0; i--){
-					let neighborOption = neighbor.options[i];
-					if(cull.includes(neighborOption)){
-						neighbor.options.splice(i, 1);
-					}
-				}
-				*/
-
-				if(neighbor.options.length === 0){
-					// deadlock -- start over
-					// TODO: write backtrack or restart mechanism
-					return false;
-				}
-
-				if(neighbor.options.length === 1){
-					neighbor.collapsed = true;
-
-					// add to collapsed array
-					this.collapsed[(neighbor.y * this.size) + neighbor.x] = neighbor;
-
-					// remove neighbor from this.uncollapsed
-					for(let i = 0; i < this.uncollapsed.length; i++){
-						if(this.uncollapsed[i].x === neighbor.x && this.uncollapsed[i].y === neighbor.y){
-							this.uncollapsed.splice(i, 1);
-							break;
-						}
-					}
-
-					continue;
-				}
-
-				// IFF neighbor has less options than it started with, add it to stack
-				//		for the changes to be propagated
-				if(neighbor.options.length < this.patternOptions.length){
-					stack.push(neighbor);
-				} 
-			}
-		}
-		
-		console.log("propagated")
-		this.printGrid();
-		return true; // no deadlocks detected
-	}
 	
-	randomIndex(arr){
+	// returns a random index from the given array
+	#randomIndex(arr){
 		let max = arr.length;
 		let pick = Math.floor(Math.random() * max);
 
@@ -204,30 +187,32 @@ class WFC // extends Phaser.Scene
 	}
 
 	// sorts ip adjacencies by pattern and direction
-	adbjByPattern(patterns){
+	//		makes something like:
+	//		[ 0: {up: [...], down: [...] }, 1: { down: [...], right: [...] } ]
+	#adjByPattern(patterns){
 		let result = [];
 		for(let p of patterns){
 			let pAdj = {};
 			for(let dir in DIRECTIONS){
 				let dirOptions = [];
 				let found = false;
-				for(let adj of this.ip.adjacencies){	
+				for(let adj of this.#ip.adjacencies){	
 					if(adj[0] === p && `${adj[2]}` === dir){
 						dirOptions.push(adj[1]);
 						found = true;
 					}	
 				}
-				if(found) pAdj[this.dirStr(dir)] = dirOptions;
+				if(found) pAdj[this.#dirStr(dir)] = dirOptions;
 			}
 			result.push(pAdj)
 		}
 		return result;
 	}
 
-	// dir = 0, 1, 2, 3	--> [ 0,    1,    2,     3]
-	// const DIRECTIONS = 	[UP, DOWN, LEFT, RIGHT];
-	// convert dir from int to string (mostly for easier debugging)
-	dirStr(dir){
+	// convert dir from int to string 
+	// 		dir = 0, 1, 2, 3	--> [ 0,    1,    2,     3]
+	// 		const DIRECTIONS = 		[UP, DOWN, LEFT, RIGHT];
+	#dirStr(dir){
 		switch(dir){
 			case "0": return "up"	
 			case "1": return "down"	
@@ -236,38 +221,14 @@ class WFC // extends Phaser.Scene
 		}
 	}
 
-	cullOptions(cellOption, dir, neighbor){
-		let cull = [];
-		// remove any neighbor option that doesnt have cell option as a lower
-		for(let neighborOption of neighbor.options){
-			let optAdj = this.patternAdjs[neighborOption];
-			
-			// if optAdj doesnt have a down option, cull neighborOption from neighbor.options
-			if(!(dir in optAdj)){ 
-				cull.push(neighborOption);
-				continue;
-			} else {
-			// optAdj does have a down option, but cellOption isn't
-			// 		one of them, cull neighborOption from neighbor.options
-				if(!optAdj[dir].includes(cellOption)){
-					cull.push(neighborOption);
-					continue;
-				}
-
-			}
-		}
-		return cull;
-	}
-
-
-	// DEBUG HELPER
-	printGrid(){
+	// (debug help) prints the collapse options of grid
+	#printGrid(){
 		let print = "";
 
-		for(let y = 0; y < this.size; y++){
-			for(let x = 0; x < this.size; x++){
-				let index = (y * this.size) + x;
-				let str = `${this.grid[index].options[0]}`;
+		for(let y = 0; y < this.#size; y++){
+			for(let x = 0; x < this.#size; x++){
+				let index = (y * this.#size) + x;
+				let str = `${this.#grid[index].options[0]}`;
 				print += str.padEnd(4);		// a minor TODO: change 4 to a variable
 			}
 			print += `\n`
@@ -276,10 +237,10 @@ class WFC // extends Phaser.Scene
 		console.log(print)
 	}
 
-	resetVisitFlag(){
-		for(let cell of this.grid){
+	// sets visit flag to false for every cell in grid
+	#resetVisitFlag(){
+		for(let cell of this.#grid){
 			cell.visited = false;
 		}
 	}
-
 }
