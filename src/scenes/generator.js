92deaf8109eec30f3815ddd880851dfd00f59e3e
mtan001 <137580499+mtan001@users.noreply.github.com>
2025-01-23 10:49:20 -0800

    generator stuff

--- src/scenes/generator.js
+++ src/scenes/generator.js
@@ -14,23 +14,6 @@ class Generator extends Phaser.Scene
 
 	create()
 	{
-		// Run wfc on the ground matrix using the image processor and the constraint solver
-			// Turn the outputted image into a new ground layer
-		this.ip.process(PATHFINDER_GROUND, this.N);
-
-		// Run wfc on the structures matrix using the image processor and the constraint solver
-			// Turn the outputted image into a new structures layer
-		this.ip.process(PATHFINDER_STRUCTURES, this.N);
-			
-		// Run wfdm on the structures layer
-
-		// Display the ground and structures layer and take a screenshot
-
-		// generate stuff
-		this.generateKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-		this.generateKey.on("down", () => {
-			// generate stuff
-		});
 		this.map = this.add.tilemap("three-farmhouses", 16, 16, 25, 40);
 
         // Add a tileset to the map
@@ -40,6 +23,37 @@ class Generator extends Phaser.Scene
         this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
         this.treesLayer = this.map.createLayer("Trees-n-Bushes", this.tileset, 0, 0);
         this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0, 0);
+
+		// Run wfc on the ground matrix using the image processor and the constraint solver
+			// Turn the outputted image into a new ground layer
+		this.ip.process(PATHFINDER_GROUND, this.N);
+		const groundMap = this.make.tilemap({
+			data: // ground tileid array
+			tileWidth: 16,
+			tileHeight: 16,
+		});
+		this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
+
+		// Run wfc on the structures matrix using the image processor and the constraint solver
+			// Turn the outputted image into a new structures layer
+		this.ip.process(PATHFINDER_STRUCTURES, this.N);
+		const structuresMap = this.make.tilemap({
+			data: // structure tileid array
+			tileWidth: 16,
+			tileHeight: 16,
+		});
+		this.structuresLayer = this.map.createLayer("Structures", this.tileset, 0, 0);
+			
+		// Run wfdm on the structures layer
+
+		// Display the ground and structures layer and take a screenshot
+
+		// generate stuff
+		this.generateKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+		this.generateKey.on("down", () => {
+			// generate stuff
+			console.log("generating map");
+		});
 		// Store the screenshot, ground and structures layer, and world facts database together
 	}
 }
