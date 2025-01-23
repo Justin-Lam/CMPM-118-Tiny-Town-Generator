994a43deafea4b4a4adf15dfdda8924b62bfbca0
mtan001 <137580499+mtan001@users.noreply.github.com>
2025-01-23 11:39:03 -0800

    layer stuff

--- src/scenes/generator.js
+++ src/scenes/generator.js
@@ -14,7 +14,7 @@ class Generator extends Phaser.Scene
 
 	create()
 	{
-		this.map = this.add.tilemap("three-farmhouses", 16, 16, 25, 40);
+		/*this.map = this.add.tilemap("three-farmhouses", 16, 16, 25, 40);
 
         // Add a tileset to the map
         this.tileset = this.map.addTilesetImage("kenney-tiny-town", "tilemap_tiles");
@@ -22,27 +22,27 @@ class Generator extends Phaser.Scene
         // Create the layers
         this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
         this.treesLayer = this.map.createLayer("Trees-n-Bushes", this.tileset, 0, 0);
-        this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0, 0);
+        this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0, 0);*/
 
 		// Run wfc on the ground matrix using the image processor and the constraint solver
 			// Turn the outputted image into a new ground layer
 		this.ip.process(PATHFINDER_GROUND, this.N);
-		const groundMap = this.make.tilemap({
+		/*const groundMap = this.make.tilemap({
 			data: // ground tileid array
 			tileWidth: 16,
 			tileHeight: 16,
 		});
-		this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
+		this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);*/
 
 		// Run wfc on the structures matrix using the image processor and the constraint solver
 			// Turn the outputted image into a new structures layer
 		this.ip.process(PATHFINDER_STRUCTURES, this.N);
-		const structuresMap = this.make.tilemap({
+		/*const structuresMap = this.make.tilemap({
 			data: // structure tileid array
 			tileWidth: 16,
 			tileHeight: 16,
 		});
-		this.structuresLayer = this.map.createLayer("Structures", this.tileset, 0, 0);
+		this.structuresLayer = this.map.createLayer("Structures", this.tileset, 0, 0);*/
 			
 		// Run wfdm on the structures layer (UNCOMMENT AND EDIT LINE BELOW WHEN STRUCTURES ARRAY IS IMPLEMENTED)
 		// this.wm = new Wfdm(structuresArray, mapWidth, mapHeight, structRange);
