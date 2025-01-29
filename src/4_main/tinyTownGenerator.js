c8fb64cc9d429a9e7cc9eb645c86381d28bbde7a
Justin Lam <justinflam9225k@gmail.com>
2025-01-28 21:18:12 -0800

    Reverting back to previous michelle map because latest one breaks constraint solver

--- src/4_main/tinyTownGenerator.js
+++ src/4_main/tinyTownGenerator.js
@@ -15,7 +15,7 @@ class TinyTownGenerator extends Phaser.Scene {
 	preload() {
 		this.load.setPath("./assets/");
 		this.load.image("tilemap_tiles", "tilemap_packed.png");
-		this.load.tilemapTiledJSON("tinyTownMap", "tinyTownMap3.tmj");
+		this.load.tilemapTiledJSON("tinyTownMap", "tinyTownMap2.tmj");
 	}
 
 	create()
