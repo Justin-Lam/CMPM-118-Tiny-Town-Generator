4bb310e1c80f06dbcf27616aa85e486ae79fe0a3
Justin Lam <justinflam9225k@gmail.com>
2025-02-10 11:18:58 -0800

    Simplified directions.js

--- src/2_utility/directions.js
+++ src/2_utility/directions.js
@@ -1,6 +1,7 @@
-// [y, x], where positive y points down and positive x points right
-const UP = [-1, 0];
-const DOWN = [1, 0];
-const LEFT = [0, -1];
-const RIGHT = [0, 1];
-const DIRECTIONS = [UP, DOWN, LEFT, RIGHT];
+/** [up, down, left, right], where each direction is [y, x], and where positive y points down and positive x points right. */
+const DIRECTIONS = [
+	[-1, 0],	// up
+	[1, 0],		// down
+	[0, -1],	// left
+	[0, 1]		// right
+];
